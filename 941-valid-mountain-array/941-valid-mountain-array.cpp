f90d249957aa995827f1f13b153bbe5ff40c805d
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-01-25 10:44:17 +0530

    Time: 31 ms (54.96%), Space: 22.4 MB (48.39%) - LeetHub

--- 941-valid-mountain-array/941-valid-mountain-array.cpp
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -5,24 +5,14 @@ public:
        int length = arr.size();
           int i = 0;
           while (i < length - 1 && arr[i] < arr[i + 1])
-          {
                i++;
-          }
           if (i == length - 1 || i == 0)
-          {
                return false;
-          }
           while (i < length - 1 && arr[i] > arr[i + 1])
-          {
                i++;
-          }
           if (i == length - 1)
-          {
                return true;
-          }
           else
-          {
                return false;
-          }
     }
 };
