442a470bbf1077d9753b0e87ce419a4227834d7d
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-01-30 11:41:50 +0530

    Time: 43 ms (38.80%), Space: 24.8 MB (92.88%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -3,14 +3,12 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        cout << n;
         if (n == 1)
             return;
         if (n == k)
             return;
         else if (k > n)
             k = k % n;
-        cout << k;
         reverse(nums.begin(), nums.end());
         reverse(nums.begin(), nums.begin() + k);
         reverse(nums.begin() + k, nums.end());
