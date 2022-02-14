00a07c651492b88913938ea9b46480163c46afd3
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-02-14 11:48:45 +0530

    Time: 28 ms (17.65%), Space: 20.9 MB (52.40%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -14,9 +14,8 @@ public:
     int checkBalanced(TreeNode* root) {
         if (root == nullptr) return 0;
         int lh = checkBalanced(root -> left);
-            if (lh == -1) return -1;
         int rh = checkBalanced(root -> right);
-        if (rh == -1) return -1;
+        if (lh == -1 || rh == -1) return -1;
         if (abs(lh - rh) > 1) return -1;
         return (1 + max(lh, rh));
     }
