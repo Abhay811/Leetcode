d4358e6478b6da26d04fddbb74575a78f4d850bc
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-02-16 20:28:01 +0530

    Time: 402 ms (43.98%), Space: 110.2 MB (77.82%) - LeetHub

--- 575-distribute-candies/575-distribute-candies.cpp
+++ 575-distribute-candies/575-distribute-candies.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int distributeCandies(vector<int>& candyType) {
-        set<int> s(candyType.begin(), candyType.end());
+        unordered_set<int> s(candyType.begin(), candyType.end());
         int n = candyType.size() / 2;
         int size = s.size();
         return size > n ? n : size;
