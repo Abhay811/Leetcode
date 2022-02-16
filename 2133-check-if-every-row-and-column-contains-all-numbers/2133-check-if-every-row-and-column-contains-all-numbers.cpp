1a419b4d012b052347385ab6966a7ca62dfd24d3
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-02-16 12:19:54 +0530

    Time: 403 ms (31.92%), Space: 93.5 MB (27.21%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) { 
-        set<int> si;
         for (int i = 0, n = matrix.size(); i < n; ++i)
         {
+            set<int> si, si2;
             for (int j = 0; j < n; ++j)
+            {
                 if (si.count(matrix[i][j]))
                     return false;
                 else si.insert(matrix[i][j]);
-            si.clear();
-            for (int j = 0; j < n; ++j)
-                if (si.count(matrix[j][i]))
-                    return false;
-                else si.insert(matrix[j][i]);
-            si.clear();
+                if (si2.count(matrix[j][i])) return false;
+                else si2.insert(matrix[j][i]);
+            }
         }
         return true;
     }
