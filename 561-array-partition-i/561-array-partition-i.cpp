8b86072a6afd4f2fa743b1ed3c18ab03f0fbd8d1
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-02-16 20:43:54 +0530

    Time: 82 ms (34.13%), Space: 28.3 MB (48.10%) - LeetHub

--- 561-array-partition-i/561-array-partition-i.cpp
+++ 561-array-partition-i/561-array-partition-i.cpp
@@ -3,8 +3,8 @@ public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int s = 0;
-        for (int i = 0, n = nums.size(), j = 1; j < n; i += 2, j += 2)
-            s += min(nums[i], nums[j]);
+        for (int i = 0, n = nums.size(); i < n; i += 2)
+            s += min(nums[i], nums[i + 1]);
         return s;
     }
 };
