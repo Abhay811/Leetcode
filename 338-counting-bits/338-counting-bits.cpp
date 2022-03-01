d3d0195ec370e31daa986737c49d5d3f33c0e6ae
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-03-01 19:17:43 +0530

    Time: 14 ms (16.41%), Space: 7.9 MB (48.88%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        int k = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            if (k * 2 == i)
+                k *= 2;
+            dp[i] = 1 + dp[i - k];
+        }
+        return dp;
+    }
+};
