f6afea42838ddb81865fd72b1d20f4d797c272be
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-03-03 09:44:10 +0530

    Time: 13 ms (12.40%), Space: 8.1 MB (10.68%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -3,12 +3,10 @@ public:
     int maximumWealth(vector<vector<int>>& acc) {
         
         int m = acc.size(), n = acc[0].size(), t = 0, s = 0;
-        for (int i = 0; i < m; i++) {
-            s = 0;
-            for (int j = 0; j < n; j++)
-                s += acc[i][j];
-            if (t < s)
-                t = s;
+        for (auto it : acc)
+        {
+            s = accumulate(it.begin(), it.end(), 0);
+            t = max (t, s);
         }
         return t;
     }
