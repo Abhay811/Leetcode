12338c03126135993082bad07eb7aec1a366134d
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-03-05 11:59:25 +0530

    Time: 32 ms (38.87%), Space: 10.7 MB (71.73%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void solve(vector<vector<int>>& graph, int temp, vector<int>& t, vector<vector<int>>& res, vector<int> &vis)
+    {
+        t.emplace_back(temp);
+        if (temp == graph.size() - 1)
+        {
+            res.emplace_back(t);
+            t.pop_back();
+            return;
+        }
+        vis[temp] = 1;
+        for (int i = 0; i < graph[temp].size(); ++i)
+        {
+            if (vis[graph[temp][i]] == 0)
+                solve(graph, graph[temp][i], t, res, vis);
+        }
+        vis[temp] = 0;
+        t.pop_back();
+    }
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> res;
+        vector<int> t;
+        int n = graph.size();
+        vector<int> vis(n, 0);
+        solve(graph, 0, t, res, vis);
+        return res;
+        
+    }
+};
