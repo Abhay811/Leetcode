583b93c69c7104c2bad1a28fe788ca6fcbcc0839
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-03-06 21:01:59 +0530

    Time: 3 ms (80.86%), Space: 6 MB (26.10%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int i = 1;
-        while (i <= n)
-        {
-            n -= i;
-            ++i;
-        }
-        return i - 1;
+        // int i = 1;
+        // while (i <= n)
+        // {
+        //     n -= i;
+        //     ++i;
+        // }
+        // return i - 1;
+        return floor(-0.5 + sqrt((double) 2 * n + 0.25));
     }
 };
