dae156cccb53cc69e25375fd0571823678199b2f
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-03-08 19:05:44 +0530

    Time: 13 ms (49.77%), Space: 10.1 MB (42.49%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.cpp
@@ -6,7 +6,7 @@ public:
         int y1 = coordinates[0][1];
         int x2 = coordinates[1][0];
         int y2 = coordinates[1][1];
-        for(int i=2;i<coordinates.size();i++)
+        for(int i = 2; i < coordinates.size(); i++)
         {
             int x = coordinates[i][0];
             int y = coordinates[i][1];
