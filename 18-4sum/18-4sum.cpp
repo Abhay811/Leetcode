5fa0b61479a21f61ec05edf28be1bf1af4acece1
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-04-09 03:13:58 +0100

    Time: 36 ms (92.28%), Space: 13.2 MB (77.45%) - LeetHub

--- 18-4sum/18-4sum.cpp
+++ 18-4sum/18-4sum.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& num, int target) {
-    vector<vector<int>> res;
-    if (num.empty()) return res;
-    sort(num.begin(),num.end());
-    for (int i = 0; i < num.size(); i++) 
-    {
-        int target_3 = target - num[i];
-        for (int j = i + 1; j < num.size(); j++) 
+        vector<vector<int>> res;
+        int size = num.size();
+        if (size < 4) return res;
+        sort(num.begin(),num.end());
+        for (int i = 0; i < size; i++) 
         {
-            int target_2 = target_3 - num[j];
-            int front = j + 1;
-            int back = num.size() - 1;
-            while(front < back) 
+            int target_3 = target - num[i];
+            for (int j = i + 1; j < size; j++) 
             {
-                int two_sum = num[front] + num[back];
-                if (two_sum < target_2) front++;
-                else if (two_sum > target_2) back--;
-                else 
+                int target_2 = target_3 - num[j];
+                int front = j + 1;
+                int back = size - 1;
+                while(front < back) 
                 {
-                    vector<int> quadruplet(4, 0);
-                    quadruplet[0] = num[i];
-                    quadruplet[1] = num[j];
-                    quadruplet[2] = num[front];
-                    quadruplet[3] = num[back];
-                    res.push_back(quadruplet);
-                    while (front < back && num[front] == quadruplet[2]) ++front;
-                    while (front < back && num[back] == quadruplet[3]) --back;
+                    int two_sum = num[front] + num[back];
+                    if (two_sum < target_2) front++;
+                    else if (two_sum > target_2) back--;
+                    else 
+                    {
+                        vector<int> quad(4, 0);
+                        quad[0] = num[i];
+                        quad[1] = num[j];
+                        quad[2] = num[front];
+                        quad[3] = num[back];
+                        res.push_back(quad);
+                        while (front < back && num[front] == quad[2]) ++front;
+                        while (front < back && num[back] == quad[3]) --back;
+                    }
                 }
+                while(j + 1 < size && num[j + 1] == num[j]) ++j;
             }
-            while(j + 1 < num.size() && num[j + 1] == num[j]) ++j;
+        while (i + 1 < size && num[i + 1] == num[i]) ++i;
         }
-    while (i + 1 < num.size() && num[i + 1] == num[i]) ++i;
-    }
     
-    return res;
+        return res;
     }
 };
