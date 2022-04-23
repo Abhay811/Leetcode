7ec385cfe26562d6d743265274f4b5461df0f084
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-04-23 05:45:10 +0100

    Time: 697 ms (5.01%), Space: 14.4 MB (53.71%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> res(amount + 1, amount + 1);
+        res[0] = 0;
+        for (auto it : coins)
+            for (int i = it; i <= amount; ++i)
+                res[i] = min(res[i], res[i - it] + 1);
+        for (auto it : res) cout << it << " ";
+        return res[amount] > amount ? -1 : res[amount];
+    }
+    
+};
