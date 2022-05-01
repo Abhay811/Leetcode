fa08a941994f57395912f12df83a96b9dae16c8c
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-05-01 06:35:01 +0100

    Time: 44 ms (100.00%), Space: 10.4 MB (100.00%) - LeetHub

---
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    long long appealSum(string s) {
+            long long res = 0, cur = 0, prev[26] = {};
+        for (int i = 0; i < s.size(); ++i) {
+            cur += i + 1 - prev[s[i] - 'a'];
+            prev[s[i] - 'a'] = i + 1;
+            res += cur;
+        }
+        return res;
+    }
+};
