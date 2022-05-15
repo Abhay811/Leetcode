f1db91650465a8df85967ac0e9814a0397a2af79
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-05-15 09:16:55 +0100

    Time: 1838 ms (5.18%), Space: 123.3 MB (6.34%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -1,17 +1,46 @@
 class Solution {
 public:
-    int dp[8][16384] = {};
-    int maxScore(vector<int>& nums, int i = 1, int mask = 0) {
-        int size = nums.size();
-        if (i > size / 2) return 0;
-        if (!dp[i][mask])
-            for (int j = 0; j < size; ++j)
-                for (auto k = j + 1; k < size; ++k)
-                {
-                    int newMask = (1 << j) + (1 << k);
-                    if ((mask & newMask) == 0)
-                        dp[i][mask] = max(dp[i][mask], i * __gcd(nums[j], nums[k]) + maxScore(nums, i + 1, mask + newMask));
-                }
-        return dp[i][mask];
-    }
+   int maxScore_v2_gcd(int a, int b) {
+	if (a % b == 0) return b;
+	return maxScore_v2_gcd(b, a % b);
+}
+
+void maxScore_v2_(int idx, vector<int>& nums, vector<bool>& visited, vector<int>& vec, int& res) {
+
+	if (idx >= nums.size()) {
+		vector<int> tmp = vec;
+		sort(tmp.begin(), tmp.end());
+		int val = 0;
+		for (int i = 1; i <= tmp.size(); ++i) {
+			val += i * tmp[i - 1];
+		}
+		res = max(res, val);
+		return;
+	}
+
+	if (visited[idx])
+		return maxScore_v2_(idx + 1, nums, visited, vec, res);
+
+	visited[idx] = true;
+
+	for (int i = idx + 1; i < nums.size(); ++i) {
+		if (visited[i]) continue;
+		visited[i] = true;
+		vec.push_back(maxScore_v2_gcd(max(nums[idx], nums[i]), min(nums[idx], nums[i])));
+		maxScore_v2_(idx + 1, nums, visited, vec, res);
+		vec.pop_back();
+		visited[i] = false;
+	}
+
+	visited[idx] = false;
+	return;
+}
+
+int maxScore(vector<int>& nums) {
+	int res = 0;
+	vector<bool> visited(nums.size(), 0);
+	vector<int> vec;
+	maxScore_v2_(0, nums, visited, vec, res);
+	return res;
+}
 };
