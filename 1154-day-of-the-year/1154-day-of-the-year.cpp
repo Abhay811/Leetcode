1d1515e7926f1d68a95c2e1b9b2c4e624da2e113
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-06-11 15:46:49 +0100

    Time: 42 ms (28.09%), Space: 7.3 MB (17.28%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.cpp
+++ 1154-day-of-the-year/1154-day-of-the-year.cpp
@@ -6,10 +6,9 @@ public:
         int yr = stoi(date.substr(0, 4));
         int month = stoi(date.substr(5, 2));
         int days = stoi(date.substr(8, 2));
-        int leap = 0;
+        int leap = 0, res = 0;
         if (yr % 400 == 0 || ((yr % 4 == 0) && (yr % 100 != 0)))
             leap = 1;
-        int res = 0;
         for (int i = 1; i < month; i++) 
             res += months[i];
         if (month > 2)
