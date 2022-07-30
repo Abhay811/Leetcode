e54694ad7cc2f31232f655a0822cc990f9dda9c1
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-07-30 17:21:21 +0100

    Time: 6 ms (94.40%), Space: 9.5 MB (86.59%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int x1 = 0, x2 = 0, mask = 0;
+         
+        for (int i : nums) {
+            x2 ^= x1 & i;
+            x1 ^= i;
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
+        }
+
+        return x1;
+    }
+};
