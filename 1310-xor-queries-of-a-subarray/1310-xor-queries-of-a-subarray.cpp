672f3f05fbbb12e6290b122d811d70dc4b5aa128
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-07-30 11:21:43 +0100

    Time: 167 ms (26.11%), Space: 41.7 MB (6.49%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        vector<int> vec(1), res;
+        int prev = 0;
+        for (auto it : arr)
+            vec.emplace_back(it ^ vec.back());
+        vec.erase(vec.begin());
+        for (auto it : vec) cout << it << " ";
+        
+        for (auto it : queries)
+            if (it[0] == 0) res.emplace_back(vec[it[1]]);
+            else
+                res.emplace_back(vec[it[0] - 1] ^ vec[it[1]]);
+        return res;
+        
+    }
+};
