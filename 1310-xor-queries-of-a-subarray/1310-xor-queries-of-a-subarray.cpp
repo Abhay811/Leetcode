fb853a3fe7cd876ceca08e24c3a56c9f98f1af09
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-07-30 11:22:21 +0100

    Time: 200 ms (10.34%), Space: 41.5 MB (9.36%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -5,13 +5,10 @@ public:
         int prev = 0;
         for (auto it : arr)
             vec.emplace_back(it ^ vec.back());
-        vec.erase(vec.begin());
-        for (auto it : vec) cout << it << " ";
-        
+        vec.erase(vec.begin());        
         for (auto it : queries)
             if (it[0] == 0) res.emplace_back(vec[it[1]]);
-            else
-                res.emplace_back(vec[it[0] - 1] ^ vec[it[1]]);
+            else res.emplace_back(vec[it[0] - 1] ^ vec[it[1]]);
         return res;
         
     }
