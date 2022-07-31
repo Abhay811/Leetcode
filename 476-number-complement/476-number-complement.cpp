0d605c9a11154b4929c0271dc9598ff7588b1d88
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-07-31 11:17:39 +0100

    Time: 0 ms (100.00%), Space: 5.8 MB (78.93%) - LeetHub

--- 476-number-complement/476-number-complement.cpp
+++ 476-number-complement/476-number-complement.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int findComplement(int num) {
         int m = num, t = 1, mask = 0;
-        while (num)
-            mask++, num >>=1;
-        while (--mask)
+        // while (num)
+        //     mask++, num >>=1;
+        // while (--mask)
+        //     t |= t << 1;
+        while (num >>= 1)
             t |= t << 1;
         return t ^ m;
     }
