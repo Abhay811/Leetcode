68f00f59a3995c7abebe5ea11cf3359299884ed4
Abhay Maurya <56768239+Abhay811@users.noreply.github.com>
2022-08-16 02:55:10 +0100

    Time: 122 ms (5.36%), Space: 10.8 MB (15.65%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -5,10 +5,7 @@ public:
         int size = s.size();
         int ind = size;
         for (int i = 0; i < size; i++)
-        {
-            mp[s[i]].first++;
-            mp[s[i]].second = i;
-        }
+            mp[s[i]].first++, mp[s[i]].second = i;
         for (auto [c, p] : mp)
             if (p.first == 1)
                 ind = min(ind, p.second);
